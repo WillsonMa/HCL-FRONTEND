b4bfca7f289223c551c6c4e6a0907767a3cc84c7
Kevin Phillips <kphillips86@gmail.com>
2017-01-26 23:02:23 -0600

    basic location autocomplete working

--- src/app/location-picker/location-picker.component.ts
+++ src/app/location-picker/location-picker.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import {MapsAPILoader} from 'angular2-google-maps/core';
+
+declare var google: any;
 
 @Component({
 	selector: "location-picker",
 	styleUrls: [ "./location-picker.css" ],
 	templateUrl: "./location-picker.html"
 })
-export class LocationPickerComponent { }
+export class LocationPickerComponent implements OnInit {
+  constructor(private mapsLoader: MapsAPILoader) { }
+
+  ngOnInit() {
+    this.mapsLoader.load()
+      .then(() => {
+        let autocomplete = new google.maps.places.Autocomplete(document.getElementById("location-picker"), {});
+        google.maps.event.addListener(autocomplete, 'place_changed', () => {
+          const place = autocomplete.getPlace();
+          console.log(place);
+        });
+      });
+  }
+}
