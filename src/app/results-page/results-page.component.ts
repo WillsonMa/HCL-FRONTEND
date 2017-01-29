1525b45992a61fa7f7027beb357726abac2e9481
Kevin Phillips <kphillips86@gmail.com>
2017-01-29 11:46:20 -0600

    reading params from URl when loading resutls page

--- src/app/results-page/results-page.component.ts
+++ src/app/results-page/results-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: "results-page",
 	styleUrls: [ "./results-page.css" ],
 	templateUrl: "./results-page.html"
 })
-export class ResultsPageComponent { }
+export class ResultsPageComponent implements OnInit, OnDestroy {
+	private sub: any;
+
+	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		this.sub = this.route.queryParams.subscribe(params => {
+			console.log(params);
+		});
+	}
+
+	ngOnDestroy() {
+		if (this.sub) {
+			this.sub.unsubscribe();
+		}
+	}
+}
