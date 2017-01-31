3f5c1a578cf209bddf6a686cc4718dc7535603e6
Kevin Phillips <kphillips86@gmail.com>
2017-01-31 16:49:55 -0600

    fixing tests

--- src/app/organization-page/organization-page.component.spec.ts
+++ src/app/organization-page/organization-page.component.spec.ts
@@ -3,28 +3,32 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from "@angular/platform-browser-dynamic/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { HttpModule } from '@angular/http';
 
 import { OrganizationPageComponent } from "./organization-page.component";
 
 describe("OrganizationPageComponent", () => {
-  let de: DebugElement;
-  let comp: OrganizationPageComponent;
-  let fixture: ComponentFixture<OrganizationPageComponent>;
+	let de: DebugElement;
+	let comp: OrganizationPageComponent;
+	let fixture: ComponentFixture<OrganizationPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed
-    .configureTestingModule({
-      declarations: [ OrganizationPageComponent ],
-      imports: [ RouterTestingModule ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed
+		.configureTestingModule({
+			declarations: [ OrganizationPageComponent ],
+			imports: [
+				RouterTestingModule,
+				HttpModule
+			]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrganizationPageComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css("h1"));
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(OrganizationPageComponent);
+		comp = fixture.componentInstance;
+		de = fixture.debugElement.query(By.css("h1"));
+	});
 
-  it("should create component", () => expect(comp).toBeDefined() );
+	it("should create component", () => expect(comp).toBeDefined() );
 });
