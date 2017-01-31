832cf197cea3cf6996d8fc2f855e307ad4260ebd
Kevin Phillips <kphillips86@gmail.com>
2017-01-31 16:28:22 -0600

    displaying organization data

---
+++ src/app/organization.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { URLSearchParams, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class OrganizationService {
+	private searchUrl: string = "http://hclapi-dev.us-east-2.elasticbeanstalk.com/organization";
+
+	constructor(private http: Http) { }
+
+	getOrganization(id: string) {
+		return this.http.get(`${this.searchUrl}/${id}`)
+		.toPromise()
+		.then(response => response.json())
+		.catch(err => console.log(err));
+	}
+
+}
