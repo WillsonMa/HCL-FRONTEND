3daa01d000322adadbb2e9fb59b4745e9e1d894f
Kevin Phillips <kphillips86@gmail.com>
2017-03-27 18:11:33 -0500

    restoring results page test file

--- src/app/results-page/results-page.component.spec.ts
+++ src/app/results-page/results-page.component.spec.ts
@@ -1,28 +1,49 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { DebugElement } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
 
-import { ServicesMultiselectComponent } from './services-multiselect.component';
+import { AgmCoreModule as GoogleMapsModule } from 'angular2-google-maps/core';
 import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect/src/multiselect-dropdown';
 
-describe('ServicesMultiselectComponent', () => {
-	let comp: ServicesMultiselectComponent;
-	let fixture: ComponentFixture<ServicesMultiselectComponent>;
+import { LocationPickerComponent } from "../location-picker/location-picker.component";
+import { ResultsPageComponent } from "./results-page.component";
+import { ServicesMultiselectComponent } from '../services-multiselect/services-multiselect.component';
+
+describe("ResultsPageComponent", () => {
+	let de: DebugElement;
+	let comp: ResultsPageComponent;
+	let fixture: ComponentFixture<ResultsPageComponent>;
 
 	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [ ServicesMultiselectComponent ],
-			imports: [ MultiselectDropdownModule, FormsModule ]
+		TestBed
+		.configureTestingModule({
+			declarations: [
+				ResultsPageComponent,
+				LocationPickerComponent,
+				ServicesMultiselectComponent
+			],
+			imports: [
+				RouterTestingModule,
+				MultiselectDropdownModule,
+				FormsModule,
+				HttpModule,
+				GoogleMapsModule.forRoot({
+					apiKey: "AIzaSyDnyjd_w7FdScc5fU1pc1DwncZOAXgeZMI",
+					libraries: ['places']
+				})
+			]
 		})
 		.compileComponents();
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(ServicesMultiselectComponent);
+		fixture = TestBed.createComponent(ResultsPageComponent);
 		comp = fixture.componentInstance;
-		fixture.detectChanges();
+		de = fixture.debugElement.query(By.css("h1"));
 	});
 
 	it("should create component", () => expect(comp).toBeDefined() );
