efd07b76878fd881efaf82a4632ece6bd4fcd445
Kevin Phillips <kphillips86@gmail.com>
2017-04-17 21:22:56 -0500

    defaulting location in location-picker from geolocation

--- src/app/location-picker/location-picker.component.ts
+++ src/app/location-picker/location-picker.component.ts
@@ -28,24 +28,67 @@ export class LocationPickerComponent implements OnInit {
 	locationChange = new EventEmitter();
 
 	ngOnInit() {
+		this.setDefaultGeoLocation();
+		this.setupGoogleMapsAutocomplete();
+	}
+
+	setDefaultGeoLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(({ coords }) => {
+				const geocoder = new google.maps.Geocoder();
+
+				var latlng = { lat: coords.latitude, lng: coords.longitude };
+				geocoder.geocode({ 'location': latlng }, ([ place ], status) => {
+					if (status === 'OK') {
+						if (place) {
+							const city = place.address_components
+								.filter(({ types }) => types[0] === 'locality')
+								.map(({ long_name }) => long_name)
+								.reduce(city => city);
+
+							const state = place.address_components
+								.filter(({ types }) => types[0] === 'administrative_area_level_1')
+								.map(({ short_name }) => short_name)
+								.reduce(state => state);
+
+							// if user typed in a location, don't override it
+							if (!this.location) {
+								this.updateLocationFromOutsideZone({
+									latitude: coords.latitude,
+									longitude: coords.longitude,
+									name: `${city}, ${state}`
+								});
+							}
+						}
+					}
+				});
+			}, (err) => {
+				console.log(`error retrieving user's coords: ${err}`);
+			});
+		}
+	}
+
+	setupGoogleMapsAutocomplete() {
 		this.mapsLoader.load()
 		.then(() => {
-			const autocomplete = new google.maps.places.Autocomplete(document.getElementById("location-picker"), {});
+			const $el = document.getElementById("location-picker");
+			const autocomplete = new google.maps.places.Autocomplete($el, {});
+
 			google.maps.event.addListener(autocomplete, 'place_changed', () => {
-				this.ngZone.run(() => {
-					const place = autocomplete.getPlace();
-					this.updateLocation(place);
+				const place = autocomplete.getPlace();
+				this.updateLocationFromOutsideZone({
+					latitude: place.geometry.location.lat(),
+					longitude: place.geometry.location.lng(),
+					name: place.formatted_address
 				});
 			});
 		});
 	}
 
-	updateLocation(place) {
-		this.location = {
-			latitude: place.geometry.location.lat(),
-			longitude: place.geometry.location.lng(),
-			name: place.formatted_address
-		};
-		this.locationChange.emit(this.location);
+	updateLocationFromOutsideZone(location) {
+		this.ngZone.run(() => {
+			this.location = location;
+			this.locationChange.emit(this.location);
+		});
 	}
 }
